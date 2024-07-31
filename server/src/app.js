6b624e3cb09aca7d8a5f30b8a9309c6c31b18213
OulehlaJan <oulehlajan@gmail.com>
2024-07-31 23:02:15 +0200

    Fix public folder path

--- server/src/app.js
+++ server/src/app.js
@@ -7,6 +7,9 @@ const app = express();
 // Serve static files from the React frontend app
 app.use(express.static(path.join(__dirname, "../client/build")));
 
+// Serve Strapi's public folder
+app.use("/public", express.static(path.join(__dirname, "public")));
+
 // Anything that doesn't match the above, send back index.html
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname + "/../client/build/index.html"));
