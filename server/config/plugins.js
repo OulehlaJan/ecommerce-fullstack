8ba529cd7c4ea2697807e6a7decacdd5c0d72f93
OulehlaJan <oulehlajan@gmail.com>
2024-07-31 04:30:24 +0200

    ADDING JWT KEY

--- server/config/plugins.js
+++ server/config/plugins.js
@@ -3,4 +3,9 @@ module.exports = ({ env }) => ({
     secretKey: env("STRIPE_SECRET_KEY"),
     publishableKey: env("STRIPE_PUBLISHABLE_KEY"),
   },
+  'users-permissions': {
+    config: {
+      jwtSecret: env('JWT_SECRET'),
+    },
+  },
 });
