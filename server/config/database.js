df8a4c97292c477e6c240c0d84bdc60f77c94fd1
OulehlaJan <oulehlajan@gmail.com>
2024-08-02 01:57:18 +0200

    Updated Database.js @ config/server.js

--- server/config/database.js
+++ server/config/database.js
@@ -1,26 +1,49 @@
-const parse = require("pg-connection-string").parse;
-const config = parse(process.env.DATABASE_URL);
+const { parse } = require("pg-connection-string");
 
-console.log("Loaded DATABASE_URL:", process.env.DATABASE_URL);
-console.log("Parsed config:", config);
+module.exports = ({ env }) => {
+  const { host, port, database, user, password } = parse(env("DATABASE_URL"));
 
-module.exports = ({ env }) => ({
-  connection: {
-    client: "postgres",
+  return {
     connection: {
-      host: config.host,
-      port: config.port,
-      database: config.database,
-      user: config.user,
-      password: config.password,
-      ssl: {
-        rejectUnauthorized: false,
+      client: "postgres",
+      connection: {
+        host,
+        port,
+        database,
+        user,
+        password,
+        ssl: { rejectUnauthorized: false },
       },
+      debug: false,
     },
-    pool: {
-      min: env.int("DATABASE_POOL_MIN", 2),
-      max: env.int("DATABASE_POOL_MAX", 10),
-    },
-    acquireConnectionTimeout: env.int("DATABASE_CONNECTION_TIMEOUT", 60000),
-  },
-});
+  };
+};
+
+// ZMĚNA ZA CONFIG Z DOUCMENTATION
+
+// const parse = require("pg-connection-string").parse;
+// const config = parse(process.env.DATABASE_URL);
+
+// console.log("Loaded DATABASE_URL:", process.env.DATABASE_URL);
+// console.log("Parsed config:", config);
+
+// module.exports = ({ env }) => ({
+//   connection: {
+//     client: "postgres",
+//     connection: {
+//       host: config.host,
+//       port: config.port,
+//       database: config.database,
+//       user: config.user,
+//       password: config.password,
+//       ssl: {
+//         rejectUnauthorized: false,
+//       },
+//     },
+//     pool: {
+//       min: env.int("DATABASE_POOL_MIN", 2),
+//       max: env.int("DATABASE_POOL_MAX", 10),
+//     },
+//     acquireConnectionTimeout: env.int("DATABASE_CONNECTION_TIMEOUT", 60000),
+//   },
+// });
--- server/config/server.js
+++ server/config/server.js
@@ -1,20 +1,38 @@
-const path = require("path");
-
 module.exports = ({ env }) => ({
-  url: env("MY_HEROKU_URL"),
-  host: env("HOST", "0.0.0.0"),
+  proxy: true,
+  host: "0.0.0.0",
   port: env.int("PORT", 1337),
+  url: env("MY_HEROKU_URL"),
   app: {
     keys: env.array("APP_KEYS"),
   },
-  webhooks: {
-    populateRelations: env.bool("WEBHOOKS_POPULATE_RELATIONS", false),
-  },
-  dirs: {
-    public: path.resolve(__dirname, "../../public"),
+  admin: {
+    auth: {
+      secret: env("ADMIN_JWT_SECRET"),
+    },
   },
 });
 
+// // ZMĚNA ZA CONFIG Z DOUCMENTATION
+
+// const path = require("path");
+
+// module.exports = ({ env }) => ({
+//   url: env("MY_HEROKU_URL"),
+//   host: env("HOST", "0.0.0.0"),
+//   port: env.int("PORT", 1337),
+//   app: {
+//     keys: env.array("APP_KEYS"),
+//   },
+//   webhooks: {
+//     populateRelations: env.bool("WEBHOOKS_POPULATE_RELATIONS", false),
+//   },
+//   dirs: {
+//     public: path.resolve(__dirname, "../../public"),
+//   },
+// });ˇ
+
+// ------------------------------------
 // module.exports = ({ env }) => ({
 //   host: env("HOST", "0.0.0.0"),
 //   port: env.int("PORT", 1337),
