427a708189dbc14712da9937d3d60bf7ad486492
OulehlaJan <oulehlajan@gmail.com>
2024-08-01 13:10:01 +0200

    Edit server.js

--- server/config/server.js
+++ server/config/server.js
@@ -1,36 +1,36 @@
-const path = require("path");
+// const path = require("path");
 
-console.log("Loading server.js...");
+// console.log("Loading server.js...");
 
-module.exports = ({ env }) => {
-  const publicPath = path.resolve(__dirname, "../../public");
+// module.exports = ({ env }) => {
+//   const publicPath = path.resolve(__dirname, "../../public");
 
-  // Log the paths to ensure they're correct
-  console.log("__dirname in config/server.js:", __dirname);
-  console.log("Resolved path to public folder:", publicPath);
+//   // Log the paths to ensure they're correct
+//   console.log("__dirname in config/server.js:", __dirname);
+//   console.log("Resolved path to public folder:", publicPath);
 
-  return {
-    host: env("HOST", "0.0.0.0"),
-    port: env.int("PORT", 1337),
-    app: {
-      keys: env.array("APP_KEYS"),
-    },
-    webhooks: {
-      populateRelations: env.bool("WEBHOOKS_POPULATE_RELATIONS", false),
-    },
-    dirs: {
-      public: publicPath,
-    },
-  };
-};
+//   return {
+//     host: env("HOST", "0.0.0.0"),
+//     port: env.int("PORT", 1337),
+//     app: {
+//       keys: env.array("APP_KEYS"),
+//     },
+//     webhooks: {
+//       populateRelations: env.bool("WEBHOOKS_POPULATE_RELATIONS", false),
+//     },
+//     dirs: {
+//       public: publicPath,
+//     },
+//   };
+// };
 
-// module.exports = ({ env }) => ({
-//   host: env("HOST", "0.0.0.0"),
-//   port: env.int("PORT", 1337),
-//   app: {
-//     keys: env.array("APP_KEYS"),
-//   },
-//   webhooks: {
-//     populateRelations: env.bool("WEBHOOKS_POPULATE_RELATIONS", false),
-//   },
-// });
+module.exports = ({ env }) => ({
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 1337),
+  app: {
+    keys: env.array("APP_KEYS"),
+  },
+  webhooks: {
+    populateRelations: env.bool("WEBHOOKS_POPULATE_RELATIONS", false),
+  },
+});
