fe0eb90b8a48ff40bcb9e9c1d13d7f4484b0b9c3
OulehlaJan <oulehlajan@gmail.com>
2024-08-02 17:22:49 +0200

    Updated database.js

--- server/config/server.js
+++ server/config/server.js
@@ -1,26 +1,43 @@
-const path = require("path");
-
 module.exports = ({ env }) => ({
   proxy: true,
-  host: env("HOST", "0.0.0.0"),
-  port: env.int("PORT", process.env.PORT || 1337),
-  url: env("MY_HEROKU_URL"),
-  app: {
-    keys: env.array("APP_KEYS"),
-  },
-  webhooks: {
-    populateRelations: env.bool("WEBHOOKS_POPULATE_RELATIONS", false),
-  },
-  dirs: {
-    public: path.resolve(__dirname, "../public"),
+  host: "0.0.0.0",
+  port: process.env.PORT,
+  url: env('MY_HEROKU_URL'),
+  app: { 
+    keys: env.array('APP_KEYS')
   },
   admin: {
     auth: {
-      secret: env("ADMIN_JWT_SECRET"),
+      secret: env('ADMIN_JWT_SECRET'),
     },
   },
-});
+})
 
+
+// --------------------------------------- W/ public directory path
+// const path = require("path");
+
+// module.exports = ({ env }) => ({
+//   proxy: true,
+//   host: env("HOST", "0.0.0.0"),
+//   port: env.int("PORT", process.env.PORT || 1337),
+//   url: env("MY_HEROKU_URL"),
+//   app: {
+//     keys: env.array("APP_KEYS"),
+//   },
+//   webhooks: {
+//     populateRelations: env.bool("WEBHOOKS_POPULATE_RELATIONS", false),
+//   },
+//   dirs: {
+//     public: path.resolve(__dirname, "../public"),
+//   },
+//   admin: {
+//     auth: {
+//       secret: env("ADMIN_JWT_SECRET"),
+//     },
+//   },
+// });
+// ---------------------------------------
 // // ZMĚNA ZA CONFIG Z DOUCMENTATION
 
 // const path = require("path");
