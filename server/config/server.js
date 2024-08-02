261c80faecb1671bd87dfbddf66955024adc74ad
OulehlaJan <oulehlajan@gmail.com>
2024-08-02 18:52:00 +0200

    Updated server.js

--- server/config/server.js
+++ server/config/server.js
@@ -1,18 +1,17 @@
 module.exports = ({ env }) => ({
   proxy: true,
   host: "0.0.0.0",
-  port: process.env.PORT,
-  url: env('MY_HEROKU_URL'),
-  app: { 
-    keys: env.array('APP_KEYS')
+  port: env.int("PORT", 1337),
+  url: env("MY_HEROKU_URL"),
+  app: {
+    keys: env.array("APP_KEYS"),
   },
   admin: {
     auth: {
-      secret: env('ADMIN_JWT_SECRET'),
+      secret: env("ADMIN_JWT_SECRET"),
     },
   },
-})
-
+});
 
 // --------------------------------------- W/ public directory path
 // const path = require("path");
