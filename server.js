be3cc8bdb9e4b2914dcabde5da73e978de26646b
OulehlaJan <oulehlajan@gmail.com>
2024-08-02 17:05:05 +0200

    Updated databse.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const path = require("path");
 const { createProxyMiddleware } = require("http-proxy-middleware");
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || process.env.EXPRESS_PORT || 5000;
 
 // Serve static files from the React app
 app.use(express.static(path.join(__dirname, "client/build")));
--- server/config/server.js
+++ server/config/server.js
@@ -1,34 +1,10 @@
-const path = require("path");
-
-module.exports = ({ env }) => ({
-  proxy: true,
-  host: env("HOST", "0.0.0.0"),
-  port: env.int("PORT", process.env.PORT || 1337),
-  url: env("MY_HEROKU_URL"),
-  app: {
-    keys: env.array("APP_KEYS"),
-  },
-  webhooks: {
-    populateRelations: env.bool("WEBHOOKS_POPULATE_RELATIONS", false),
-  },
-  dirs: {
-    public: path.resolve(__dirname, "../public"),
-  },
-  admin: {
-    auth: {
-      secret: env("ADMIN_JWT_SECRET"),
-    },
-  },
-});
-
-// // ZMĚNA ZA CONFIG Z DOUCMENTATION
-
 // const path = require("path");
 
 // module.exports = ({ env }) => ({
-//   url: env("MY_HEROKU_URL"),
+//   proxy: true,
 //   host: env("HOST", "0.0.0.0"),
-//   port: env.int("PORT", 1337),
+//   port: env.int("PORT", process.env.PORT || 1337),
+//   url: env("MY_HEROKU_URL"),
 //   app: {
 //     keys: env.array("APP_KEYS"),
 //   },
@@ -36,9 +12,33 @@ module.exports = ({ env }) => ({
 //     populateRelations: env.bool("WEBHOOKS_POPULATE_RELATIONS", false),
 //   },
 //   dirs: {
-//     public: path.resolve(__dirname, "../../public"),
+//     public: path.resolve(__dirname, "../public"),
 //   },
-// });ˇ
+//   admin: {
+//     auth: {
+//       secret: env("ADMIN_JWT_SECRET"),
+//     },
+//   },
+// });
+
+// // ZMĚNA ZA CONFIG Z DOUCMENTATION
+
+const path = require("path");
+
+module.exports = ({ env }) => ({
+  url: env("MY_HEROKU_URL"),
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 1337),
+  app: {
+    keys: env.array("APP_KEYS"),
+  },
+  webhooks: {
+    populateRelations: env.bool("WEBHOOKS_POPULATE_RELATIONS", false),
+  },
+  dirs: {
+    public: path.resolve(__dirname, "../../public"),
+  },
+});ˇ
 
 // ------------------------------------
 // module.exports = ({ env }) => ({
