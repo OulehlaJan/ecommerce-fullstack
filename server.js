99b1bb7f153f9239e77f08e245a6a1775133eab6
OulehlaJan <oulehlajan@gmail.com>
2024-08-02 20:27:31 +0200

    Updated package.json

--- server.js
+++ server.js
@@ -27,24 +27,6 @@ app.use(
   })
 );
 
-// // Proxy for API
-// app.use(
-//   "/api",
-//   createProxyMiddleware({
-//     target: "http://localhost:1337",
-//     changeOrigin: true,
-//   })
-// );
-
-// // Proxy admin requests to Strapi
-// app.use(
-//   "/admin",
-//   createProxyMiddleware({
-//     target: "http://localhost:1337",
-//     changeOrigin: true,
-//   })
-// );
-
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "client/build/index.html"));
 });
@@ -52,47 +34,3 @@ app.get("*", (req, res) => {
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
-
-// ------------------------------
-// const strapi = require("@strapi/strapi");
-// const path = require("path");
-// const express = require("express");
-
-// const app = express();
-
-// // Serve static files from the React frontend app
-// const clientBuildPath = path.join(__dirname, "client/build");
-// app.use(express.static(clientBuildPath));
-
-// // Serve static files from the Strapi public folder
-// const publicPath = path.join(__dirname, "public"); // Updated path
-// app.use(express.static(publicPath));
-
-// console.log(`Serving static files from ${clientBuildPath}`);
-// console.log(`Serving static files from ${publicPath}`);
-
-// // Additional logging for debugging
-// console.log("__dirname:", __dirname);
-// console.log("clientBuildPath:", clientBuildPath);
-// console.log("publicPath:", publicPath);
-
-// // Anything that doesn't match the above, send back index.html
-// app.get("*", (req, res) => {
-//   res.sendFile(path.join(clientBuildPath, "index.html"));
-// });
-
-// // Start Strapi
-// strapi({
-//   dir: path.resolve(__dirname, "server"),
-//   appPath: path.resolve(__dirname, "server"),
-//   staticPaths: {
-//     public: path.resolve(__dirname, "public"), // Updated path
-//   },
-// })
-//   .start()
-//   .then(() => {
-//     console.log("Strapi server started successfully");
-//   })
-//   .catch((err) => {
-//     console.error("Strapi server failed to start", err);
-//   });
