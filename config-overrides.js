2e56fa52a932626e811fd9eb783b00c16de2239a
OulehlaJan <oulehlajan@gmail.com>
2024-08-03 11:21:49 +0200

    package.json change

--- config-overrides.js
+++ config-overrides.js
@@ -1,9 +1,37 @@
-module.exports = function override(config, env) {
-  if (env === "development") {
-    config.devServer = {
-      ...config.devServer,
-      allowedHosts: "all",
+const fs = require("fs");
+const NodePolyfillPlugin = require("node-polyfill-webpack-plugin");
+const evalSourceMapMiddleware = require("react-dev-utils/evalSourceMapMiddleware");
+const redirectServedPath = require("react-dev-utils/redirectServedPathMiddleware");
+const noopServiceWorkerMiddleware = require("react-dev-utils/noopServiceWorkerMiddleware");
+
+module.exports = {
+  webpack: {
+    plugins: {
+      add: [
+        new NodePolyfillPlugin({
+          excludeAliases: ["console"],
+        }),
+      ],
+    },
+  },
+  devServer: (devServerConfig, { env, paths }) => {
+    devServerConfig = {
+      ...devServerConfig,
+      setupMiddlewares: (middlewares, devServer) => {
+        if (!devServer) {
+          throw new Error("webpack-dev-server is not defined");
+        }
+        if (fs.existsSync(paths.proxySetup)) {
+          require(paths.proxySetup)(devServer.app);
+        }
+        middlewares.push(
+          evalSourceMapMiddleware(devServer),
+          redirectServedPath(paths.publicUrlOrPath),
+          noopServiceWorkerMiddleware(paths.publicUrlOrPath)
+        );
+        return middlewares;
+      },
     };
-  }
-  return config;
+    return devServerConfig;
+  },
 };
