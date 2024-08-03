d87a29a844c8b1be205ad5aadfc7eb1c3de3a413
OulehlaJan <oulehlajan@gmail.com>
2024-08-03 18:01:21 +0200

    Added Cloudinary

--- server/config/plugins.js
+++ server/config/plugins.js
@@ -3,9 +3,17 @@ module.exports = ({ env }) => ({
     secretKey: env("STRIPE_SECRET_KEY"),
     publishableKey: env("STRIPE_PUBLISHABLE_KEY"),
   },
-  'users-permissions': {
+  "users-permissions": {
     config: {
-      jwtSecret: env('JWT_SECRET'),
+      jwtSecret: env("JWT_SECRET"),
+    },
+  },
+  upload: {
+    provider: "cloudinary",
+    providerOptions: {
+      cloud_name: env("CLOUDINARY_NAME"),
+      api_key: env("CLOUDINARY_KEY"),
+      api_secret: env("CLOUDINARY_SECRET"),
     },
   },
 });
