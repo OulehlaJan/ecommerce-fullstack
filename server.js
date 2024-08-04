5c9f3766c8c27e807b17fef760cade1ef707e0f1
OulehlaJan <oulehlajan@gmail.com>
2024-08-04 21:55:11 +0200

    Edit package.json

--- server.js
+++ server.js
@@ -1,39 +1,39 @@
-const { createProxyMiddleware } = require("http-proxy-middleware");
-const express = require("express");
-const path = require("path");
-const app = express();
-const PORT = process.env.PORT || 5000;
-const STRAPI_URL =
-  process.env.NODE_ENV === "production"
-    ? process.env.MY_HEROKU_URL
-    : "http://localhost:1337";
+// const { createProxyMiddleware } = require("http-proxy-middleware");
+// const express = require("express");
+// const path = require("path");
+// const app = express();
+// const PORT = process.env.PORT || 5000;
+// const STRAPI_URL =
+//   process.env.NODE_ENV === "production"
+//     ? process.env.MY_HEROKU_URL
+//     : "http://localhost:1337";
 
-// // Serve static files from the React app
-app.use(express.static(path.join(__dirname, "client/build")));
+// // // Serve static files from the React app
+// app.use(express.static(path.join(__dirname, "client/build")));
 
-// Proxy for API
-app.use(
-  "/api",
-  createProxyMiddleware({
-    target: STRAPI_URL,
-    changeOrigin: true,
-    pathRewrite: { "^/api": "" },
-  })
-);
+// // Proxy for API
+// app.use(
+//   "/api",
+//   createProxyMiddleware({
+//     target: STRAPI_URL,
+//     changeOrigin: true,
+//     pathRewrite: { "^/api": "" },
+//   })
+// );
 
-// Proxy admin requests to Strapi
-app.use(
-  "/admin",
-  createProxyMiddleware({
-    target: STRAPI_URL,
-    changeOrigin: true,
-  })
-);
+// // Proxy admin requests to Strapi
+// app.use(
+//   "/admin",
+//   createProxyMiddleware({
+//     target: STRAPI_URL,
+//     changeOrigin: true,
+//   })
+// );
 
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "client/build/index.html"));
-});
+// app.get("*", (req, res) => {
+//   res.sendFile(path.join(__dirname, "client/build/index.html"));
+// });
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+// app.listen(PORT, () => {
+//   console.log(`Server is running on port ${PORT}`);
+// });
