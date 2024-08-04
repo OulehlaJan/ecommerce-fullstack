9dbd272a173ee8c2c574a1d715c47f285e443d01
OulehlaJan <oulehlajan@gmail.com>
2024-08-04 22:47:22 +0200

    test

--- server.js
+++ server.js
@@ -1,3 +1,46 @@
+const express = require("express");
+const { exec } = require("child_process");
+const path = require("path");
+
+const app = express();
+
+// Serve the static files from the React app
+app.use(express.static(path.join(__dirname, "client/build")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client/build", "index.html"));
+});
+
+// Start Strapi
+exec(
+  "npm run start:server",
+  { cwd: path.join(__dirname, "server") },
+  (err, stdout, stderr) => {
+    if (err) {
+      console.error(`Error starting Strapi: ${stderr}`);
+    } else {
+      console.log(`Strapi started: ${stdout}`);
+    }
+  }
+);
+
+// Start Nginx
+exec(
+  "nginx -c " + path.join(__dirname, "nginx/nginx.conf"),
+  (err, stdout, stderr) => {
+    if (err) {
+      console.error(`Error starting Nginx: ${stderr}`);
+    } else {
+      console.log(`Nginx started: ${stdout}`);
+    }
+  }
+);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
 // const { createProxyMiddleware } = require("http-proxy-middleware");
 // const express = require("express");
 // const path = require("path");
