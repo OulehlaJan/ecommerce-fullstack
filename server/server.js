5500ade15ee19fb7b1743b7b3f0f2b97308742e6
OulehlaJan <oulehlajan@gmail.com>
2024-08-04 20:05:29 +0200

    Edit package.json

--- server/server.js
+++ server/server.js
@@ -1,2 +1,22 @@
-const strapi = require("@strapi/strapi");
-strapi().start();
+const strapi = require("strapi");
+const path = require("path");
+const express = require("express");
+
+strapi().start(() => {
+  const app = strapi.app;
+
+  // Serve static files from the React app
+  app.use(express.static(path.join(__dirname, "../client/build")));
+
+  // Catch-all to send back React's index.html for any other routes
+  app.use((req, res, next) => {
+    if (req.originalUrl.startsWith("/admin")) {
+      next();
+    } else {
+      res.sendFile(path.join(__dirname, "../client/build", "index.html"));
+    }
+  });
+});
+
+// const strapi = require("@strapi/strapi");
+// strapi().start();
