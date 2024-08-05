4ded1f0332ba7ad4d59da6a3ff6b49a0a8417e6d
OulehlaJan <oulehlajan@gmail.com>
2024-08-05 17:57:15 +0200

    changed structure

---
+++ config/database.js
@@ -0,0 +1,24 @@
+const { parse } = require("pg-connection-string");
+
+module.exports = ({ env }) => {
+  const { host, port, database, user, password } = parse(env("DATABASE_URL"));
+
+  return {
+    connection: {
+      client: "postgres",
+      connection: {
+        host,
+        port,
+        database,
+        user,
+        password,
+        ssl: { rejectUnauthorized: false },
+      },
+      pool: {
+        min: 0,
+        max: 5, // snížený limit připojení
+      },
+      debug: false,
+    },
+  };
+};
