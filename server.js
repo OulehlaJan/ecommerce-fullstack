09199219eae57ae3848a0c0227784d2e23860219
OulehlaJan <oulehlajan@gmail.com>
2024-08-05 04:38:35 +0200

    test

--- server.js
+++ server.js
@@ -1,28 +1,23 @@
-const { createProxyMiddleware } = require("http-proxy-middleware");
 const express = require("express");
-const { spawn } = require("child_process");
+const { createProxyMiddleware } = require("http-proxy-middleware");
 const path = require("path");
+const cors = require("cors");
 const app = express();
 const PORT = process.env.PORT || 5000;
-const STRAPI_URL = process.env.STRAPI_URL || "http://localhost:1337";
+const STRAPI_PORT = process.env.STRAPI_PORT || 1337; // Strapi bude běžet na tomto portu
+const STRAPI_URL = `http://localhost:${STRAPI_PORT}`; // Interní URL pro Strapi
 
-// Přidání vlastních CORS hlaviček
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Methods",
-    "GET, POST, PUT, PATCH, DELETE, OPTIONS"
-  );
-  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  next();
-});
-
-// Spuštění Strapi serveru jako samostatného procesu
-const strapiProcess = spawn("npm", ["start"], {
-  stdio: "inherit",
-  cwd: path.resolve(__dirname, "server"),
-  shell: true,
-});
+// Nastavení CORS middleware
+app.use(
+  cors({
+    origin: [
+      "http://localhost:3000",
+      "https://stylish-one-7f1f35e5b636.herokuapp.com",
+    ],
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 
 // Proxy for API
 app.use(
@@ -32,14 +27,6 @@ app.use(
     changeOrigin: true,
     pathRewrite: { "^/api": "" },
     logLevel: "debug",
-    onProxyRes: function (proxyRes, req, res) {
-      res.header("Access-Control-Allow-Origin", "*");
-      res.header(
-        "Access-Control-Allow-Methods",
-        "GET, POST, PUT, PATCH, DELETE, OPTIONS"
-      );
-      res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-    },
   })
 );
 
@@ -50,14 +37,6 @@ app.use(
     target: STRAPI_URL,
     changeOrigin: true,
     logLevel: "debug",
-    onProxyRes: function (proxyRes, req, res) {
-      res.header("Access-Control-Allow-Origin", "*");
-      res.header(
-        "Access-Control-Allow-Methods",
-        "GET, POST, PUT, PATCH, DELETE, OPTIONS"
-      );
-      res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-    },
   })
 );
 
@@ -69,15 +48,103 @@ app.get("*", (req, res) => {
 });
 
 app.listen(PORT, () => {
-  console.log("Server is running on port " + PORT);
+  console.log(`Server is running on port ${PORT}`);
+  // Spuštění Strapi serveru jako samostatného procesu
+  const { spawn } = require("child_process");
+  const strapiProcess = spawn("npm", ["run", "start:server"], {
+    stdio: "inherit",
+    cwd: path.resolve(__dirname, "server"),
+    shell: true,
+  });
+
+  // Handle cleanup when the process is terminated
+  process.on("SIGINT", () => {
+    strapiProcess.kill("SIGINT");
+    process.exit();
+  });
 });
 
-// Handle cleanup when the process is terminated
-process.on("SIGINT", () => {
-  strapiProcess.kill("SIGINT");
-  process.exit();
-});
+// const { createProxyMiddleware } = require("http-proxy-middleware");
+// const express = require("express");
+// const { spawn } = require("child_process");
+// const path = require("path");
+// const app = express();
+// const PORT = process.env.PORT || 5000;
+// const STRAPI_URL = process.env.STRAPI_URL || "http://localhost:1337";
 
+// // Přidání vlastních CORS hlaviček
+// app.use((req, res, next) => {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header(
+//     "Access-Control-Allow-Methods",
+//     "GET, POST, PUT, PATCH, DELETE, OPTIONS"
+//   );
+//   res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+//   next();
+// });
+
+// // Spuštění Strapi serveru jako samostatného procesu
+// const strapiProcess = spawn("npm", ["start"], {
+//   stdio: "inherit",
+//   cwd: path.resolve(__dirname, "server"),
+//   shell: true,
+// });
+
+// // Proxy for API
+// app.use(
+//   "/api",
+//   createProxyMiddleware({
+//     target: STRAPI_URL,
+//     changeOrigin: true,
+//     pathRewrite: { "^/api": "" },
+//     logLevel: "debug",
+//     onProxyRes: function (proxyRes, req, res) {
+//       res.header("Access-Control-Allow-Origin", "*");
+//       res.header(
+//         "Access-Control-Allow-Methods",
+//         "GET, POST, PUT, PATCH, DELETE, OPTIONS"
+//       );
+//       res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+//     },
+//   })
+// );
+
+// // Proxy admin requests to Strapi
+// app.use(
+//   "/admin",
+//   createProxyMiddleware({
+//     target: STRAPI_URL,
+//     changeOrigin: true,
+//     logLevel: "debug",
+//     onProxyRes: function (proxyRes, req, res) {
+//       res.header("Access-Control-Allow-Origin", "*");
+//       res.header(
+//         "Access-Control-Allow-Methods",
+//         "GET, POST, PUT, PATCH, DELETE, OPTIONS"
+//       );
+//       res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+//     },
+//   })
+// );
+
+// // Serve static files from the React app
+// app.use(express.static(path.join(__dirname, "client/build")));
+
+// app.get("*", (req, res) => {
+//   res.sendFile(path.join(__dirname, "client/build/index.html"));
+// });
+
+// app.listen(PORT, () => {
+//   console.log("Server is running on port " + PORT);
+// });
+
+// // Handle cleanup when the process is terminated
+// process.on("SIGINT", () => {
+//   strapiProcess.kill("SIGINT");
+//   process.exit();
+// });
+
+// ˇ-------------------------------
 // const { createProxyMiddleware } = require("http-proxy-middleware");
 // const express = require("express");
 // const { spawn } = require("child_process");
