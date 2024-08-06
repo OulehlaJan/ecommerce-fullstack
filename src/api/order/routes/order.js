87ba218ade5f83c14bde89fd16edc24f2ccbee29
OulehlaJan <oulehlajan@gmail.com>
2024-08-06 04:10:51 +0200

    Updated checkout success path

--- src/api/order/routes/order.js
+++ src/api/order/routes/order.js
@@ -1,9 +1,24 @@
-'use strict';
+"use strict";
 
 /**
  * order router
  */
 
-const { createCoreRouter } = require('@strapi/strapi').factories;
+const { createCoreRouter } = require("@strapi/strapi").factories;
 
-module.exports = createCoreRouter('api::order.order');
+const defaultRouter = createCoreRouter("api::order.order");
+
+const customRoutes = [
+  {
+    method: "POST",
+    path: "/order/webhook",
+    handler: "order.handleWebhook",
+    config: {
+      auth: false,
+    },
+  },
+];
+
+module.exports = {
+  routes: [...defaultRouter.routes, ...customRoutes],
+};
