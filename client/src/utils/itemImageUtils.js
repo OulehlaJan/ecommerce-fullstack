0e75ac70bf5b10be257d4a39d25a3668147923cc
OulehlaJan <oulehlajan@gmail.com>
2024-08-06 20:20:20 +0200

    Edited itemImageUtils.js

--- client/src/utils/itemImageUtils.js
+++ client/src/utils/itemImageUtils.js
@@ -1,20 +1,14 @@
 import DefaultImage from "../assets/defaultImage/defaultImage.png";
 
 export const getItemImageUrl = (item) => {
+  // Výchozí URL pro obrázek, pokud položka nemá přiřazený obrázek
   const defaultImageUrl = DefaultImage;
 
-  if (item?.attributes?.image?.data?.attributes?.formats?.medium?.url) {
-    const imageUrl = item.attributes.image.data.attributes.formats.medium.url;
-
-    // Kontrola, zda URL již obsahuje Cloudinary základní URL
-    if (imageUrl.startsWith('http')) {
-      return imageUrl;
-    }
-
-    return `https://stylish-one-7f1f35e5b636.herokuapp.com${imageUrl}`;
-  }
-
-  return defaultImageUrl;
+  // Získání URL obrázku nebo výchozího obrázku
+  return (
+    item?.attributes?.image?.data?.attributes?.formats?.medium?.url ||
+    defaultImageUrl
+  );
 };
 
 // import DefaultImage from "../assets/defaultImage/defaultImage.png";
