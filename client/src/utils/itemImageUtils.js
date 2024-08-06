e9ad585e6d936c9fb5125b6e0de46f2bf6989694
OulehlaJan <oulehlajan@gmail.com>
2024-08-06 20:07:24 +0200

    Updated dependencies

--- client/src/utils/itemImageUtils.js
+++ client/src/utils/itemImageUtils.js
@@ -1,11 +1,30 @@
 import DefaultImage from "../assets/defaultImage/defaultImage.png";
 
 export const getItemImageUrl = (item) => {
-  const defaultImageUrl = { DefaultImage };
+  const defaultImageUrl = DefaultImage;
 
   if (item?.attributes?.image?.data?.attributes?.formats?.medium?.url) {
-    return `https://stylish-one-7f1f35e5b636.herokuapp.com${item.attributes.image.data.attributes.formats.medium.url}`;
+    const imageUrl = item.attributes.image.data.attributes.formats.medium.url;
+
+    // Kontrola, zda URL již obsahuje Cloudinary základní URL
+    if (imageUrl.startsWith('http')) {
+      return imageUrl;
+    }
+
+    return `https://stylish-one-7f1f35e5b636.herokuapp.com${imageUrl}`;
   }
 
   return defaultImageUrl;
 };
+
+// import DefaultImage from "../assets/defaultImage/defaultImage.png";
+
+// export const getItemImageUrl = (item) => {
+//   const defaultImageUrl = { DefaultImage };
+
+//   if (item?.attributes?.image?.data?.attributes?.formats?.medium?.url) {
+//     return `https://stylish-one-7f1f35e5b636.herokuapp.com${item.attributes.image.data.attributes.formats.medium.url}`;
+//   }
+
+//   return defaultImageUrl;
+// };
--- server/server.js
+++ server/server.js
@@ -1,13 +1,2 @@
 const strapi = require("@strapi/strapi");
-
-console.log("Starting Strapi...");
-
-// Ověření načtení environment variables
-console.log("Cloudinary config:", {
-  cloud_name: process.env.CLOUDINARY_NAME,
-  api_key: process.env.CLOUDINARY_KEY,
-  api_secret: process.env.CLOUDINARY_SECRET,
-});
-
 strapi().start();
-console.log("Strapi has started.");
