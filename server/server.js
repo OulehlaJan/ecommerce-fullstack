527caada309f2a1f94adbb4a0ccabd644051dd20
OulehlaJan <oulehlajan@gmail.com>
2024-08-06 19:44:51 +0200

    Updated dependencies

--- server/server.js
+++ server/server.js
@@ -1,2 +1,13 @@
 const strapi = require("@strapi/strapi");
+
+console.log("Starting Strapi...");
+
+// Ověření načtení environment variables
+console.log("Cloudinary config:", {
+  cloud_name: process.env.CLOUDINARY_NAME,
+  api_key: process.env.CLOUDINARY_KEY,
+  api_secret: process.env.CLOUDINARY_SECRET,
+});
+
 strapi().start();
+console.log("Strapi has started.");
