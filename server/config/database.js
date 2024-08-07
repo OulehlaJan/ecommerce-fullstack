a735d2c419d186e3da56f227249f9a358d745afe
OulehlaJan <oulehlajan@gmail.com>
2024-08-07 03:39:19 +0200

    Added security

--- server/config/database.js
+++ server/config/database.js
@@ -2,6 +2,7 @@ const { parse } = require("pg-connection-string");
 
 module.exports = ({ env }) => {
   const { host, port, database, user, password } = parse(env("DATABASE_URL"));
+  const sslConfig = env("DATABASE_SSL") ? JSON.parse(env("DATABASE_SSL")) : false;
 
   return {
     connection: {
@@ -12,7 +13,7 @@ module.exports = ({ env }) => {
         database,
         user,
         password,
-        ssl: { rejectUnauthorized: false },
+        ssl: sslConfig,
       },
       pool: {
         min: 0,
