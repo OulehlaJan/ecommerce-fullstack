724d5c8156b7f7c2e0698561d4705144e0ee1d52
OulehlaJan <oulehlajan@gmail.com>
2024-08-07 16:26:24 +0200

    Added preview URL

--- client/src/api/config.js
+++ client/src/api/config.js
@@ -1,4 +1,9 @@
-export const API_URL =
-  process.env.NODE_ENV === "production"
-    ? process.env.REACT_APP_API_URL_PRODUCTION
-    : process.env.REACT_APP_API_URL_DEVELOPMENT;
+export const API_URL = (() => {
+  if (process.env.NODE_ENV === "production") {
+    return process.env.REACT_APP_API_URL_PRODUCTION;
+  } else if (windows.location.hostname === "localhost") {
+    return process.env.REACT_APP_API_URL_DEVELOPMENT;
+  } else {
+    return process.env.REACT_APP_API_URL_PREVIEW;
+  }
+})();
