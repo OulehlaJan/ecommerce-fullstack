a4100c4671108a2100e4951e8f48ffb4b91de140
OulehlaJan <oulehlajan@gmail.com>
2024-08-07 17:11:22 +0200

    Fixed netlify.toml

--- client/src/api/config.js
+++ client/src/api/config.js
@@ -3,8 +3,7 @@ export const API_URL = (() => {
     return process.env.REACT_APP_API_URL_PRODUCTION;
   } else if (window.location.hostname === "localhost") {
     return process.env.REACT_APP_API_URL_DEVELOPMENT;
-  } else {
+  } else if (process.env.NODE_ENV === "preview") {
     return process.env.REACT_APP_API_URL_PREVIEW;
   }
 })();
-// test
