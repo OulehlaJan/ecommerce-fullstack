06af1e99e3387c50267e7dc8eedd9ebdc15ebef3
OulehlaJan <oulehlajan@gmail.com>
2024-08-07 17:51:16 +0200

    Edited config.js API

--- client/src/api/config.js
+++ client/src/api/config.js
@@ -1,9 +1,9 @@
 export const API_URL = (() => {
-  if (window.location.hostname === "localhost") {
-    return process.env.REACT_APP_API_URL_DEVELOPMENT;
+  if (process.env.REACT_APP_ENV === "production") {
+    return process.env.REACT_APP_API_URL_PRODUCTION;
   } else if (process.env.REACT_APP_ENV === "preview") {
     return process.env.REACT_APP_API_URL_PREVIEW;
   } else {
-    return process.env.REACT_APP_API_URL_PRODUCTION;
+    return process.env.REACT_APP_API_URL_DEVELOPMENT;
   }
 })();
