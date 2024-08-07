ae3250d49ae61217fc9eaa2bc0f6510896460fbe
OulehlaJan <oulehlajan@gmail.com>
2024-08-07 17:26:21 +0200

    Edited middlewares

--- client/src/api/config.js
+++ client/src/api/config.js
@@ -3,7 +3,8 @@ export const API_URL = (() => {
     return process.env.REACT_APP_API_URL_PRODUCTION;
   } else if (window.location.hostname === "localhost") {
     return process.env.REACT_APP_API_URL_DEVELOPMENT;
-  } else if (process.env.NODE_ENV === "preview") {
+  } else {
     return process.env.REACT_APP_API_URL_PREVIEW;
   }
 })();
+// test
