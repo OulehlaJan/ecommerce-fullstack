b362def23a163f2fe97d152df5bce041fc8af188
OulehlaJan <oulehlajan@gmail.com>
2024-08-07 08:51:17 +0200

    Increased swipeScrollTolerance

--- client/src/scenes/home/MainCarousel.jsx
+++ client/src/scenes/home/MainCarousel.jsx
@@ -42,7 +42,7 @@ const MainCarousel = () => {
         showThumbs={false}
         showIndicators={false}
         showStatus={false}
-        swipeScrollTolerance={60}
+        swipeScrollTolerance={75}
         preventMovementUntilSwipeScrollTolerance={true}
         renderArrowPrev={(onClickHandler, hasPrev, label) => (
           <IconButton
