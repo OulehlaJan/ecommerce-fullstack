9071ba9f928baa8e0b9fb0f75cfd0e51d71f8d1a
Jan Oulehla <oulehlajan@gmail.com>
2025-01-03 20:16:17 +0100

    Update server.js

--- server/config/server.js
+++ server/config/server.js
@@ -1,17 +1,10 @@
-module.exports = ({ env }) => {
-  console.log('🚀 Server running on HOST:', env('HOST', '0.0.0.0'), 'PORT:', env.int('PORT', 1337));
-
-  return {
-    proxy: true,
-    host: env('HOST', '0.0.0.0'),
-    port: env.int('PORT', 1337),
-    app: {
-      keys: env.array('APP_KEYS'),
-    },
-    admin: {
-      auth: {
-        secret: env('ADMIN_JWT_SECRET'),
-      },
-    },
-  };
-};
+module.exports = ({ env }) => ({
+  proxy: true,
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 1337),
+  app: {
+    keys: env.array("APP_KEYS"),
+  },
+  admin: {
+    auth: {
+      secret: env("ADMIN_JWT_SECRET"),
